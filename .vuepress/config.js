37a7e39a480b21e861f3614d806fdbc64b5d95f9
Leemagination <328445543@qq.com>
2020-07-24 14:58:50 +0800

    修改vuepress配置文件,增加travis配置文件

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
+    base:'/projectIntroduction/', 
     themeConfig:{
         search: false,
         sidebar: [
